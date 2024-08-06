240e11f0123fdcf7a527f0c27a848fbb4887cc20
Cassio Eduardo Alves Lopes <cassiollopes11@gmail.com>
2024-08-06 17:10:17 -0300

    Changes to be committed: 	modified:   backend/src/app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -113,7 +113,7 @@ app.post('/not', (req, res) => {
     payment
       .search({ qs: filtro })
       .then((res) => {
-        const pagamento = res.body.results[0]
+        const pagamento = res.results[0]
 
         if(!pagamento) return console.log('Pagamento inexistente!')
 
