4f2826c2d192aab1c3b671376800c3b5249d55bc
Cassio Eduardo Alves Lopes <cassiollopes11@gmail.com>
2024-08-06 14:26:01 -0300

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const jwKey = 'fdadfdsafasgiujq-498ut';
+const cors = require('cors');
+const { Payment, MercadoPagoConfig } = require('mercadopago');
+
+app.use(express.json());
+app.use(cors());
+
+const client = new MercadoPagoConfig({
+  accessToken:
+    'APP_USR-5142954928151152-080412-f0ae0dde25cad8b7689e7318abed1e2f-529572815',
+});
+
+const payment = new Payment(client);
+
+mongoose
+  .connect('mongodb://localhost:27017/guiapics')
+  .then()
+  .catch((err) => console.log(err));
+
+const user = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model('User', user);
+
+app.get('/', (req, res) => {
+  res.send('oi');
+});
+
+app.post('/user', async (req, res) => {
+  let { name, email, password } = req.body;
+
+  if (!name || !email || !password)
+    return res.status(400), res.send('Algum parametro esta vazio.');
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) return res.status(400), res.send('Email já cadastrado.');
+
+    password = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+
+    return res.status(200), res.send('Usuário criado com sucesso!');
+  } catch (err) {
+    return res.status(500), res.send('Erro interno');
+  }
+});
+
+app.delete('/user/:email', async (req, res) => {
+  try {
+    await User.findOneAndDelete({ email: req.params.email });
+    return res.status(200), res.send('Usuário deletado.');
+  } catch (err) {
+    return res.status(500), res.send('Erro interno.');
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404), res.send('Email nao cadastrado');
+
+  const passValid = await bcrypt.compare(password, user.password);
+  if (!passValid) return res.status(400), res.send('Senha incorreta');
+
+  const token = jwt.sign({ email }, jwKey, { expiresIn: '24h' });
+
+  return res.status(200), res.send(token);
+});
+
+app.post('/payment', async (req, res) => {
+
+  const { email, cpf, amount, description } = req.body
+
+  payment
+    .create({
+      body: {
+        transaction_amount: parseFloat(amount),
+        description: description,
+        payment_method_id: 'pix',
+        notification_url: 'http://165.22.177.121:8000/',
+        payer: {
+          email: email,
+          identification: {
+            type: 'CPF',
+            number: cpf,
+          },
+        },
+      },
+      requestOptions: { idempotencyKey: Date.now() },
+    })
+    .then((result) => res.send(result))
+    .catch((error) => res.send(error));
+});
+
+module.exports = app;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const formNull = {
+    amount: '',
+    description: '',
+    email: '',
+    cpf: '',
+  };
+
+  const [form, setForm] = useState(formNull);
+  const [link, setLink] = useState('');
+
+  const submit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:8000/payment', {...form})
+      .then((response) => {
+        console.log(response.data)
+        setLink(response.data.point_of_interaction.transaction_data.ticket_url);
+        setForm(formNull)
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={submit}
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <input
+          type="number"
+          value={form.amount}
+          onChange={(e) => setForm({ ...form, amount: e.target.value })}
+          placeholder="Preco"
+        />
+        <input
+          type="text"
+          value={form.description}
+          onChange={(e) => setForm({ ...form, description: e.target.value })}
+          placeholder="Descricao"
+        />
+        <input
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          placeholder="Email"
+        />
+        <input
+          type="number"
+          name="cpf"
+          value={form.cpf}
+          onChange={(e) => setForm({ ...form, cpf: e.target.value })}
+          placeholder="Cpf"
+        />
+        <button>Create Payment</button>
+      </form>
+      {link && (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '440px',
+            height: '820px',
+            background: '#222',
+            padding: '1rem'
+          }}
+        >
+          <iframe src={link} style={{width: '100%', height: '100%'}}></iframe>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
